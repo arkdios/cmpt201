91cf7b8d1dd1b72ea6477a4346f87d2c0cc5966a
Tuan Huynh <158622871+arkdios@users.noreply.github.com>
2024-10-16 20:07:24 +0000

    complete q3

--- lab05/base2dec.c
+++ lab05/base2dec.c
@@ -14,31 +14,43 @@ int main(int argc, char* argv[]){
     }
 
     int base = atoi(argv[2]); //base of the number
-    int num = atoi(argv[1]); //the number xb
-    char alp[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    int con; //the converted number
-    int y = 0; //decimal form of the base b
+    int num = 0;//the number xb
+    int y = 0;
 
-    printf("%i\n", base);
-    printf("%i\n", num);
     //Assigning numerical values to letters
     //First loop every single character of argv[1]
     for(int i = 0; i < strlen(argv[1]); i++){
-        //Second loop for looping the alphabet
-        for(int n = 0; n < 26; n++){
-            if(argv[1][i] == alp[n]){
-                con = 10 + n;
-                printf("%i\n", con);
-            }
+        num = 0;
+
+        switch(argv[1][i]){
+            case('A'):
+            num = 10;
+            break;
+
+            case('B'):
+            num = 11;
+            break;
+
+            case('C'):
+            num = 12;
+            break;
+
+            case('D'):
+            num = 13;
+            break;
+
+            case('E'):
+            num = 14;
+            break;
+            case('F'):
+            num = 15;
+            break;
+
+            default:
+            num = (int) (argv[1][i] - '0');
         }
-        if(num != 0){
-            y += con*pow(base, strlen(argv[1])-(i+1));
-            printf("%i\n", y);
-        }
-        else{
-            y += num*pow(base, strlen(argv[1])-(i+1));
-            printf("%i\n", y);
-        }
+        
+        y += num*pow(base, strlen(argv[1])-(i+1));
     }
 
     //Outputs:
